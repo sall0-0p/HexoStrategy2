c085114e4cf7da7d2dbb6e5e579ba1a51fc874ab
_lordBucket <antoniuk.orest@gmail.com>
2025-05-17 21:35:43 +0200

    I do not trust ChatGPT, verified coordinates.

--- src/shared/classes/CubePosition.ts
+++ src/shared/classes/CubePosition.ts
@@ -1,10 +1,10 @@
 export const CUBE_DIRECTIONS: [number, number, number][] = [
-    [1, -1, 0],
     [1, 0, -1],
-    [0, 1, -1],
-    [-1, 1, 0],
     [-1, 0, 1],
+    [1, -1, 0],
+    [-1, 1, 0],
     [0, -1, 1],
+    [0, 1, -1],
 ];
 
 export class CubePosition {
