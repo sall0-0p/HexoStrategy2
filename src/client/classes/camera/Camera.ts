03dadb241d7a36a1458f4b78534221e011ba316c
_lordBucket <antoniuk.orest@gmail.com>
2025-05-19 22:07:55 +0200

    Basic Camera Angle, Removed unnecessary print.

---
+++ src/client/classes/camera/Camera.ts
@@ -0,0 +1,72 @@
+import {RunService, UserInputService, Workspace} from "@rbxts/services";
+
+export class Camera {
+    private cameraPart: Part;
+    private currentCamera;
+    private cameraSpeed = 30 // Per tick;
+
+    // tatgets
+    private targetPosition = Vector3.zero;
+
+    private static instance: Camera;
+    private constructor() {
+        const part = new Instance("Part");
+        part.Size = new Vector3(1, 1, 1);
+        part.Anchored = true;
+        part.CanCollide = false;
+        part.Position = new Vector3(0, 2, 0);
+        part.Parent = Workspace;
+        this.cameraPart = part;
+
+        this.currentCamera = Workspace.CurrentCamera!;
+        this.currentCamera.CameraType = Enum.CameraType.Scriptable;
+        // this.currentCamera.CameraSubject = this.cameraPart;
+
+        RunService.RenderStepped.Connect((delta) => this.onRender(delta))
+        this.onRender(0.01);
+    }
+
+    private processArrowKeys(delta: number) {
+        let directionVector = new Vector3(0, 0 ,0);
+        if (UserInputService.IsKeyDown(Enum.KeyCode.Up)) {
+            directionVector = directionVector.add(new Vector3(0, 0, 1));
+        }
+
+        if (UserInputService.IsKeyDown(Enum.KeyCode.Down)) {
+            directionVector = directionVector.add(new Vector3(0, 0, -1));
+        }
+
+        if (UserInputService.IsKeyDown(Enum.KeyCode.Left)) {
+            directionVector = directionVector.add(new Vector3(1, 0, 0));
+        }
+
+        if (UserInputService.IsKeyDown(Enum.KeyCode.Right)) {
+            directionVector = directionVector.add(new Vector3(-1, 0, 0));
+        }
+
+        const adjustedSpeed = this.cameraSpeed
+        const positionChange = directionVector.mul(-adjustedSpeed * delta);
+        // this.cameraPart.Position = this.cameraPart.Position.add(positionChange);
+        this.targetPosition = this.cameraPart.Position.add(positionChange);
+        this.lerpPositionToTarget(delta);
+    }
+
+    private lerpPositionToTarget(delta: number) {
+        const alpha = 1 - math.exp(this.cameraSpeed * delta);
+        this.cameraPart.Position = this.cameraPart.Position.Lerp(this.targetPosition, alpha);
+    }
+
+    private onRender(delta: number) {
+        this.processArrowKeys(delta);
+
+        const cameraCFrame = new CFrame(this.cameraPart.Position.add(new Vector3(0, 10, 0)));
+        this.currentCamera.CFrame = cameraCFrame.mul(CFrame.Angles(math.rad(45), math.rad(180), 0));
+    }
+
+    public static getInstance() {
+        if (!this.instance) {
+            this.instance = new Camera();
+        }
+        return this.instance;
+    }
+}
--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,13 +1,12 @@
-import {ReplicatedStorage} from "@rbxts/services";
 import {HexRepository} from "./classes/hex/HexRepository";
 import {NationRepository} from "./classes/nation/NationRepository";
 import {HeatmapManager} from "./classes/heatmap/HeatmapManager";
 import {NationHeatmap} from "./classes/heatmap/heatmaps/NationHeatmap";
-import {UnitReplicatorMessage} from "../shared/dto/UnitReplicatorMessage";
 import {UnitRepository} from "./classes/unit/UnitRepository";
 import {UnitFlairManager} from "./classes/unit/flair/UnitFlairManager";
-// import {Camera} from "./classes/camera/Camera";
+import {Camera} from "./classes/camera/Camera";
 
+const camera = Camera.getInstance();
 const nationRepository = NationRepository.getInstance();
 const hexRepository = HexRepository.getInstance();
 const unitRepository = UnitRepository.getInstance();
@@ -15,4 +14,3 @@ const unitFlairManager = UnitFlairManager.getInstance();
 hexRepository.getLoadedSignal().wait();
 const heatmapManager = HeatmapManager.getInstance();
 heatmapManager.showHeatmap(new NationHeatmap());
-// const camera = Camera.getInstance();
