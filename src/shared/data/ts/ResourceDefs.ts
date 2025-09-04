f5dce2c3add1924103d2d535ecfb021ffcd751ff
_lordBucket <antoniuk.orest@gmail.com>
2025-09-04 19:38:52 +0200

    [Feature] New resource icons.

--- src/shared/data/ts/ResourceDefs.ts
+++ src/shared/data/ts/ResourceDefs.ts
@@ -9,37 +9,37 @@ import {
 export const ResourceDefs: Record<ResourceType, ResourceDef> = {
     [ResourceType.Steel]: {
         name: "Steel",
-        icon: "rbxassetid://109018347927070",
+        icon: "rbxassetid://100113446854818",
         layoutOrder: 1,
     },
     [ResourceType.Oil]: {
         name: "Oil",
-        icon: "rbxassetid://116220862621129",
+        icon: "rbxassetid://92821626956496",
         layoutOrder: 2,
     },
     [ResourceType.Aluminium]: {
         name: "Aluminium",
-        icon: "rbxassetid://100381251028112",
+        icon: "rbxassetid://72161205517059",
         layoutOrder: 3,
     },
     [ResourceType.Rubber]: {
         name: "Rubber",
-        icon: "rbxassetid://108807152636823",
+        icon: "rbxassetid://81984590782381",
         layoutOrder: 4,
     },
     [ResourceType.Electronics]: {
         name: "Electronics",
-        icon: "rbxassetid://81856628649317",
+        icon: "rbxassetid://114754949273831",
         layoutOrder: 5,
     },
     [ResourceType.Alloys]: {
         name: "Rare Alloys",
-        icon: "rbxassetid://70430714365340",
+        icon: "rbxassetid://95601836379910",
         layoutOrder: 6,
     },
     [ResourceType.Uranium]: {
         name: "Uranium",
-        icon: "rbxassetid://96174045205912",
+        icon: "rbxassetid://87045635631128",
         layoutOrder: 7,
     }
 }
